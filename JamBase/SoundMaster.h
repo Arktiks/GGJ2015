b351bc26eac4a6682e97d964d969d798fee2ac71
Mestari VKarjalainen <Nuudelitlautasella@hotmail.com>
2015-01-24 20:47:04 +0200

    Rikkoo
    Kaiken
    KAIKEN

--- JamBase/SoundMaster.h
+++ JamBase/SoundMaster.h
@@ -8,14 +8,14 @@ class SoundMaster
 {
 public:
 	SoundMaster();
-	sf::SoundBuffer* AddSound(std::string name, std::string filepath);
+	void AddSound(std::string name, std::string filepath);
 	sf::SoundBuffer* FindSound(std::string name);
 	void LoadMusic(std::string name, std::string filepath);
 	void setCurrentMusic(std::string name);
 	~SoundMaster();
 	sf::Music* currentMusic;
 private:
-	std::map<std::string, sf::SoundBuffer*> sounds;
+	std::map<std::string, sf::SoundBuffer> sounds;
 	std::map<std::string, sf::Music*> musics;
 };
 
